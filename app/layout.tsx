0ef618e74655ba7211ef79902f0b78728252a4af
Rohit kumar yadav <rohitkuyada@gmail.com>
2024-11-14 12:53:23 +0530

    Add service worker for offline support and create manifest file

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,17 +34,17 @@ export const metadata: Metadata = {
     'attendance optimization'
   ],
   referrer: 'origin-when-cross-origin',
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
-    { media: '(prefers-color-scheme: dark)', color: '#000000' }
-  ],
-  colorScheme: 'light dark',
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
+  // themeColor: [
+  //   { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+  //   { media: '(prefers-color-scheme: dark)', color: '#000000' }
+  // ],
+  // colorScheme: 'light dark',
+  // viewport: {
+  //   width: 'device-width',
+  //   initialScale: 1,
+  //   maximumScale: 1,
+  //   userScalable: false,
+  // },
   
   // Icons
   icons: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+"use client";
 import { BunkMasterProComponent } from '@/components/bunk-master-pro';
 import { Analytics } from "@vercel/analytics/react"
+import { useEffect } from 'react';
 
 import React from 'react'
 
 const Page = () => {
+  useEffect(() => {
+    if (!navigator.serviceWorker.controller) {
+      navigator.serviceWorker.register("/service-worker.js").then(function (reg) {
+        console.log("Service worker has been registered for scope: " + reg.scope);
+      });
+    }
+  }, [])
+
   return (
     <div>
+
       <Analytics />
       <BunkMasterProComponent />
     </div>
